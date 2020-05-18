60fb5dabcfe6f83be6d4d10e772bedcbcf856903
Matthew <matt.toronto97@gmail.com>
2020-05-18 15:53:58 -0400

    Blueprint updates

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,26 +1,27 @@
-from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, jsonify
-from flask_login import login_user, logout_user, current_user, login_required
-from config import Config
 
-from werkzeug.urls import url_parse
-from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
+from app import db
+from app.main.forms import LoginForm, RegistrationForm, EditProfileForm, \
     ResetPasswordRequestForm, ResetPasswordForm, authorIndexQueryForm
 from app.models import User, Query
 from app.email import send_password_reset_email
+from app.main import bp
+
+from app.main_api_functions import *
+
+from datetime import datetime, timezone
+from flask import render_template, flash, redirect, url_for, request, jsonify
+from flask_login import login_user, logout_user, current_user, login_required
+from config import Config
+from werkzeug.urls import url_parse
 import itertools
 import re
 import ast
 import datetime
-from datetime import timezone
 import pandas as pd
-from app import app
-from app.main_api_functions import *
 from collections import Counter
-#from tqdm.notebook import tqdm
 from geotext import GeoText
 import time
+
 
 def make_string_from_dict(input_dict):
     output_str = ''
@@ -36,47 +37,47 @@ def make_string_from_dict2(input_dict):
     output_str = output_str[:-1]
     return output_str
 
-@app.before_request
+@bp.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.datetime.now(timezone.utc)
         db.session.commit()
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('main.login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('main.index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
-@app.route('/logout')
+@bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -84,49 +85,49 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
+        return redirect(url_for('main.login'))
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/reset_password_request', methods=['GET', 'POST'])
+@bp.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = ResetPasswordRequestForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
             send_password_reset_email(user)
         flash('Check your email for the instructions to reset your password')
-        return redirect(url_for('login'))
+        return redirect(url_for('main.login'))
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
 
 
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+@bp.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     user = User.verify_reset_password_token(token)
     if not user:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = ResetPasswordForm()
     if form.validate_on_submit():
         user.set_password(form.password.data)
         db.session.commit()
         flash('Your password has been reset.')
-        return redirect(url_for('login'))
+        return redirect(url_for('main.login'))
     return render_template('reset_password.html', form=form)
 
 
-@app.route('/user/<username>')
+@bp.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()    
     return render_template('user.html', user=user)
 
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm(current_user.username)
@@ -135,7 +136,7 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
+        return redirect(url_for('main.edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
@@ -144,7 +145,7 @@ def edit_profile():
 
 
 
-@app.route('/query/<query_type>', methods=['GET', 'POST'])
+@bp.route('/query/<query_type>', methods=['GET', 'POST'])
 #@login_required
 def make_a_query(query_type):
     if query_type == 'author_papers':
@@ -152,7 +153,7 @@ def make_a_query(query_type):
     elif query_type == 'author_affils':
         form = authorIndexQueryForm()      
 
-    if form.validate_on_submit() and current_user.is_authenticated:
+    if form.validate_on_submit() :
         query = Query(query_type = query_type,
                     query_text = form.query_text.data, 
                     query_from = form.query_from.data,
@@ -169,7 +170,6 @@ def make_a_query(query_type):
                                                 n_authors = 25, 
                                                 affils = query.query_affiliations, 
                                                 api_key = form.api_key.data)
-        author_keywords = {author : author_dict['pubtypes_'] for author, author_dict in results.items()}
         return render_template('query_results.html', json_data = results)
 
     return render_template('make_a_query.html', form=form)
@@ -179,7 +179,7 @@ def make_a_query(query_type):
 #######
 
 
-@app.route('/api/help/', methods = ['GET'])
+@bp.route('/api/help/', methods = ['GET'])
 def help():
     return {'endpoints' : {'/api/query/author_affils/' : {'parameters' : 
                                                 {'query' : '', 'from' : '', 'locations' : '', 'n' : ''}, 'info' : ''},
@@ -189,7 +189,7 @@ def help():
             'general_notes' : 'chris smells'}
 
 
-@app.route('/api/query/author_affils/', methods = ['GET'])
+@bp.route('/api/query/author_affils/', methods = ['GET'])
 def query_author_affils():
 
     timeit_start = time.time()
@@ -209,13 +209,13 @@ def query_author_affils():
         abort(404)
     
     timeit_end = time.time()
-    app.logger.info(f'`author_affils_w_location` for "{query}" from {from_year} onward ran in {round(timeit_end - timeit_start, 4)} seconds. Returning results.')
+    print(f'`author_affils_w_location` for "{query}" from {from_year} onward ran in {round(timeit_end - timeit_start, 4)} seconds. Returning results.')
     #print(f'`author_affils_w_location` for "{query}" from {from_year} onward ran in {round(timeit_end - timeit_start, 4)} seconds. Returning results.')
 
     return jsonify(author_df.to_dict('records'))
 
 
-@app.route('/api/query/author_papers/', methods = ['GET'])
+@bp.route('/api/query/author_papers/', methods = ['GET'])
 
 def query_author_papers(query = "", from_year = "", locations = "", n_authors = "", affils = "", api_key = ""):
 
@@ -243,5 +243,5 @@ def query_author_papers(query = "", from_year = "", locations = "", n_authors = 
 
     timeit_end = time.time()
     #print(f'`author_papers_w_location` for "{query}" from {from_year} onward ran in {round(timeit_end - timeit_start,4)} seconds. Returning results.')
-    app.logger.info(f'`author_papers_w_location` for "{query}" from {from_year} onward ran in {round(timeit_end - timeit_start,4)} seconds. Returning results.')
+    print(f'`author_papers_w_location` for "{query}" from {from_year} onward ran in {round(timeit_end - timeit_start,4)} seconds. Returning results.')
     return out_dict
--- fal.py
+++ fal.py
@@ -1,5 +1,7 @@
-from app import app, db
-from app.models import User
+from app import create_app, db
+from app.models import User, Query
+
+app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
