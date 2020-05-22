b30f777902edbd07310900414ffb0745d36777b1
Matthew <matt.toronto97@gmail.com>
2020-05-22 09:08:31 -0400

    tweaks

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        "postgresql://postgres:qwertyboII626@localhost/flask_app"
+        "postgresql:///flask_app"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
